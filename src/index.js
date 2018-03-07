25a9c5938bded71ddf2ff4c8a68e18a5c0b51860
Jeffrey Zhen <jzhen91@gmail.com>
2018-03-07 18:34:06 -0500

    create component structures

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDnPveBFU8XPxr4BkiZ1GTGXX-oDUNnOaI';
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
